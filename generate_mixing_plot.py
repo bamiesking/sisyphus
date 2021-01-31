0dd7fa5d46c34bd2aef0543afdd21eca7aff90c6
Ben Amies-King <bamiesking@gmail.com>
2021-01-31 00:38:48 +0000

    Updated generate_mixing_plot.py and generate_zeeman_plot.py

--- generate_mixing_plot.py
+++ generate_mixing_plot.py
@@ -1,4 +1,4 @@
-from sisyphus import BField, Atom, A_hfs, get_orbital_symbol
+from sisyphus import Field, Atom, A_hfs, get_orbital_symbol
 from scipy.constants import physical_constants
 import numpy as np
 import matplotlib.pyplot as plt
@@ -11,8 +11,8 @@ i = 2 # n=2, l=0, F=1, mF=0 line
 # Define B field 
 profile = np.array([lambda x: 0,
                     lambda y: 0,
-                    lambda z: z*A_hfs/physical_constants['Bohr magneton'][0]])
-B = BField(profile)
+                    lambda z: z*A_hfs[1]/physical_constants['Bohr magneton'][0]])
+B = Field(profile)
 
 fig = plt.figure()
 ax = fig.add_subplot(111)
@@ -26,8 +26,15 @@ lines = atom1.plotZeemanEnergyShift(n)
 # atom2 = Atom(2, 1, B_field=B)
 # lines += atom2.plotZeemanEnergyShift(n)
 
-mixing = atom1.calculateStateMixing(n, 0, 3)
-norm = plt.Normalize(mixing.min(), mixing.max())
+mixing = atom1.calculateStateMixing(n)
+vals = np.full((n.size, mixing.shape[-1]), 0)
+norm = plt.Normalize(0, 1)
+
+for j in range(mixing.shape[-1]):
+    vals[:,j] = mixing[:, j, i]
+    # vals[:,j] = np.einsum('ij,kj->i', mixing[:, i, :], mixing[:, j, :])
+    # vals[:,j] = np.tensordot(mixing[0, i, :], mixing[:,j,:], axes=(0, 1))
+# vals = np.divide(vals, np.linalg.norm(vals, axis=1)[:,None])
 
 
 for line,j in zip(lines, range(len(lines))):
@@ -36,8 +43,8 @@ for line,j in zip(lines, range(len(lines))):
     else:
         line.set_cmap('viridis')
         line.set_norm(norm)
-        line.set_array(mixing[:, j, i])
-    ax.add_collection(line)
+        line.set_array((vals[:, j]**2))
+        ax.add_collection(line)
 
 line = lines[0]
 fig.colorbar(line, ax=ax)
@@ -47,10 +54,10 @@ fig.colorbar(line, ax=ax)
 
 
 # Setup plot
-ax.set_title(r'Zeeman shifts for $^1H{{{n}}}^{{{S}}}{{{l}}}$ levels'
-             .format(n=atom1.n,
-                     l=get_orbital_symbol(atom1.l),
-                     S=int(2*atom1.s+1)))
+# ax.set_title(r'Zeeman shifts for $^1H{{{n}}}^{{{S}}}{{{l}}}$ levels'
+#              .format(n=atom1.n,
+#                      l=get_orbital_symbol(atom1.l),
+#                      S=int(2*atom1.s+1)))
 
 ax.set_xlabel(r'$\frac{\mu_B B}{A_{hfs}}$', fontsize=12)
 ax.set_ylabel(r'$\frac{E}{A_{hfs}}$', fontsize=12, rotation=0)
--- generate_zeeman_plot.py
+++ generate_zeeman_plot.py
@@ -1,4 +1,4 @@
-from sisyphus import BField, Atom, A_hfs, get_orbital_symbol
+from sisyphus import Field, Atom, A_hfs, get_orbital_symbol
 from scipy.constants import physical_constants
 import numpy as np
 import matplotlib.pyplot as plt
@@ -7,27 +7,20 @@ import matplotlib.pyplot as plt
 # Define B field 
 profile = np.array([lambda x: 0,
                     lambda y: 0,
-                    lambda z: z*A_hfs/physical_constants['Bohr magneton'][0]])
-B = BField(profile)
+                    lambda z: z*A_hfs[1]/physical_constants['Bohr magneton'][0]])
+B = Field(profile)
 
 fig = plt.figure()
 ax = fig.add_subplot(111)
 fig.subplots_adjust(bottom=0.15)
 
-n = np.arange(0, 1, 0.01)
-ax.set_prop_cycle(color=['blue'])
-atom1 = Atom(2, B_field=B)
-lines = atom1.plotZeemanEnergyShift(n)
-for line in lines:
-    ax.add_collection(line)
-
-
-
-# Setup plot
-# ax.set_title(r'Zeeman shifts for $^1H{{{n}}}^{{{S}}}{{{l}}}$ levels'
-#              .format(n=atom1.n,
-#                      l=get_orbital_symbol(atom1.l),
-#                      S=int(2*atom1.s+1)))
+n = 2
+x = np.arange(0, 0.025*1, 0.0001)
+for l in range(n-1, n):
+    atom = Atom(n, l, B_field=B)
+    lines = atom.plotZeemanEnergyShift(x)
+    for line, j in zip(lines, range(len(lines))):
+        ax.add_collection(line)
 
 ax.set_xlabel(r'$\frac{\mu_B B}{A_{hfs}}$', fontsize=12)
 ax.set_ylabel(r'$\frac{E}{A_{hfs}}$', fontsize=12, rotation=0)
